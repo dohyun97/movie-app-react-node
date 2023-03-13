ed23b89d8305deb36c3165ee7b3f88f59b50743c
dohyun97 <dohyun647@gmail.com>
2023-03-13 19:36:58 -0400

    with react page

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+
+import './App.css';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
+import LandingPage from "./components/views/LandingPage/LandingPage";
+import LoginPage from "./components/views/LoginPage/LoginPage";
+import RegisterPage from "./components/views/RegisterPage/RegisterPage";
+
+function App() {
+  return (
+    <Router>
+      <div>
+       
+
+        <Routes>
+          <Route exact path="/" element = {<LandingPage />} />
+          <Route path="/login" element = {<LoginPage />} />
+          <Route path="/register" element = {<RegisterPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
+
